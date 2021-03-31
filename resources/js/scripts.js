c55a338e833f6c81a422a87d962eb9f533f7c8f6
graefmf <magr7489@colorado.edu>
2021-03-30 21:30:10 -0600

    nfg#001

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -9,5 +9,37 @@ function getAssets(){
 
     $.ajax({url:url, dataType:"json"}).then(function(data) {
         console.log(data);
+
+
+        var row = '<div class="row">';
+        var title;
+        var owner;
+        var image;
+        var num_assets = data.assets.length;
+        for(var i = 0; i < num_assets; i++){
+            title = data.assets[i].name;
+            image = data.assets[i].image_url;
+            owner = data.assets[i].owner.user.username;
+
+            row += `<div class="col-sm">
+                <div class="card" style="width: 20rem;">
+                    <img class="card-img-top" src="${image}" alt="No Poster Found">
+                    <div class="card-body">
+                        <h5 class="card-title">${title}</h5>
+                        <h5 class="card-title">${owner}</h5>
+                    </div>
+                </div>
+            </div>`;
+
+
+            if(i == num_movies-1){
+                row += '</div>';
+            }else if((i+1)%6 == 0){
+                row += '</div>';
+                row += '<div class="row">';
+            }
+            
+        }
+       $("#Solar System").html(row);
     });
 }
