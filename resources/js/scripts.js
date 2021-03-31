135a75fd2fbdd19200342dddd53f96ac493f4a37
graefmf <magr7489@colorado.edu>
2021-03-30 22:44:09 -0600

    added api call

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -22,7 +22,7 @@ function getAssets(){
             owner = data.assets[0].owner.user.username;
             link = data.assets[0].permalink;
 
-            deck += `<div class="card">
+            deck += `<div class="card" style="width: 20rem">
                         <a href="${link}">
                             <img class="card-img-top" src="${image}" alt="No image found" width="256" height="256">
                         </a>
