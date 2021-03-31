0b803c421113cc6c324e025a741b31af18e025be
graefmf <magr7489@colorado.edu>
2021-03-30 21:20:43 -0600

    nfg#001

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,22 +1,13 @@
 function getAssets(){
     $("#Solar System").html("");
 
-    const fetch = require('node-fetch');
+    order_direction =  'desc',
+    offset = '0',
+    limit = '20'
 
-    const url = 'https://api.opensea.io/api/v1/assets';
+    const url = `https://api.opensea.io/api/v1/assets?order_direction=${direction}&offset=${offset}&limit=${limit}&collection=non-fungible-galaxy`;
 
-    const options = {
-        method: 'GET',
-        qs: {
-            order_direction: 'desc',
-            offset: '0',
-            limit: '20',
-            collection: 'non-fungible-galaxy'
-        }
-    };
-
-    fetch(url, options)
-        .then(res => res.json())
-        .then(json => console.log(json))
-        .catch(err => console.error('error:' + err));
+    $.ajax({url:url, dataType:"json"}).then(function(data) {
+        console.log(data);
+    })
 }
