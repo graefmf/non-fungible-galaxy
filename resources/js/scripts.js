75bd1bc82caea8ee87626aefebc47343448bf680
graefmf <magr7489@colorado.edu>
2021-03-31 00:01:13 -0600

    added api call

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,5 +1,4 @@
-function getAssets(collection){
-    $('#'+ collection).html("");
+function getAssets(){
 
     order_direction =  'desc';
     offset = '0';
@@ -11,36 +10,34 @@ function getAssets(collection){
         console.log(data);
 
 
-        var deck = '<div class="row">';
         var owner;
         var link;
         var image;
         var group;
         var num_assets = data.assets.length;
+
         for(var i = 0; i < num_assets; i++){
             title = data.assets[i].name;
             image = data.assets[i].image_url;
             owner = data.assets[i].owner.user.username;
             link = data.assets[i].permalink;
             group = data.assets[i].traits[6].value;
-
-            if(group == collection){
-
-                deck += `<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
-                            <div class="card">
-                                <a href="${link}">
-                                    <img class="card-img-top" src="${image}" alt="No image found" style="width: 20rem;">
-                                </a>
-                                <div class="card-body text-center">
-                                    <div class="text">
-                                        <h5 class="card-title">Owner: ${owner}</h5>
-                                    </div>
+            console.log('#'+ group);
+            $('#'+ group).append( 
+                    `<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
+                        <div class="card">
+                            <a href="${link}">
+                                <img class="card-img-top" src="${image}" alt="No image found" style="width: 20rem;">
+                            </a>
+                            <div class="card-body text-center">
+                                <div class="text">
+                                    <h5 class="card-title">Owner: ${owner}</h5>
                                 </div>
                             </div>
-                        </div>`;
-            }
+                        </div>
+                    </div>`
+            );
+            
         }
-        deck += `</div>`;
-       $('#'+ collection).html(deck);
     });
 }
